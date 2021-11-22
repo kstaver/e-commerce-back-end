30553f585b7739a393ff3d730a03473a3fee8a78
Kendra Staver <staverkendra@gmail.com>
2021-11-21 23:48:09 -0800

    fixed comments

--- models/Product.js
+++ models/Product.js
@@ -6,7 +6,7 @@ const sequelize = require('../config/connection');
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
-// set up fields and rules for Product model
+// Set up fields and rules for Product model
 Product.init(
   {
     // Define id column
@@ -33,7 +33,7 @@ Product.init(
         isDecimal: true
       }
     },
-    // define stock column
+    // Define stock column
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
