b7282cade028731647fc28c025327f76dd51f6b6
Kendra Staver <staverkendra@gmail.com>
2021-11-21 23:37:30 -0800

    created tag belongstomany

--- models/index.js
+++ models/index.js
@@ -19,9 +19,13 @@ Product.belongsToMany(Tag,{
   through: ProductTag,
   foreignKey: 'product_id',
   otherKey: 'tag_id'
-})
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
