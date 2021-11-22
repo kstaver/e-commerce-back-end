964cc517d43d95e424e9b9043079df1d81e7474a
Kendra Staver <staverkendra@gmail.com>
2021-11-21 23:36:04 -0800

    created pro has to many tags

--- models/index.js
+++ models/index.js
@@ -15,6 +15,11 @@ Category.hasMany(Product,{
 });
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag,{
+  through: ProductTag,
+  foreignKey: 'product_id',
+  otherKey: 'tag_id'
+})
 
 // Tags belongToMany Products (through ProductTag)
 
