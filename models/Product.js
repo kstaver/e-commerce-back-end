b2d5ad5d8a4329b2ab1d3bd96f076314276382d7
Kendra Staver <staverkendra@gmail.com>
2021-11-21 23:47:40 -0800

    Created columns

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,46 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    // Define id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // Define product name column
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate:{
+        len: [1,255]
+      }
+    },
+    // Define price column
+    price: {
+      type: DataTypes.DECIMAL(10,2),
+      allowNull: false,
+      validate:{
+        isDecimal: true
+      }
+    },
+    // define stock column
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate:{
+        isNumeric: true
+      }
+    },
+    // Define category id column
+    category_id: {
+      type: DataTypes.INTEGER,
+      references:{
+        model: "category",
+        key: "id"
+      }
+    }
   },
   {
     sequelize,
