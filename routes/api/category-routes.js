72fbd18c7adea16072a0fe5dad9d2356fa9f47f5
Kendra Staver <staverkendra@gmail.com>
2021-12-23 21:02:27 -0800

    fixed typo

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -49,7 +49,7 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
   // create a new category
   Category.create({
-    catagory_name: req.body.category_name
+    category_name: req.body.category_name
   }).then(dbCategoryData => res.json(dbCategoryData))
     .catch(err => {
       console.log(err);
